0c16c8d936501444da11000c3aee4bda509f4ebc
hfoley <30324719+hfoley@users.noreply.github.com>
2021-03-16 13:08:01 -0400

    Update 02 - GrantStorageRights.ps1
    comment update

--- 01 Create Resources/02 - GrantStorageRights.ps1
+++ 01 Create Resources/02 - GrantStorageRights.ps1
@@ -1,14 +1,9 @@
 ﻿
 <# 
 
-Update all the variables in the section below for your environment.  
-Update the values below within the < > 
-
-** After you run this you'll need to update the linked services created. 
-** You'll need to update the storage key for ADLSGen2LnkSvr 
-** You'll need to update the user/password for Azure SQL and Synapse SQL pool
-** Make sure to create the logging table prior to running pipelines
-** Make sure to run the 01-UpdateADFJsonTemplate script first as files created used in this process
+This will grant storage blob contributor role for mananged service account 
+created with Synapse.  It will also grant admin AAD account in the 
+paramfile that right as well.  (Could be redundant...I know...but it's needed)
 #>
 param(
         [Parameter(Mandatory)]
